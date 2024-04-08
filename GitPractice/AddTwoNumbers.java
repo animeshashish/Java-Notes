cf29a5146f5c5cfaa33e6f85045e02c9e9c3f371
Animesh Hemrom <animeshashishhemrom@gmail.com>
2024-04-08 06:46:12 +0530

    Git Practice file Added

---
+++ GitPractice/AddTwoNumbers.java
@@ -0,0 +1,12 @@
+package GitPractice;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 25;
+        int sum = a+b;
+
+        System.out.println("Sum of two numbers a+b =" + " " + sum);
+    }
+}
